057d2678a7f2015c227aa09395e4ab46f769e483
unknown <kadibaf@yahoo.com>
2017-11-07 18:52:29 -0500

    removing service workers

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render((
